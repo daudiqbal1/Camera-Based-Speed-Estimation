cc864609ee4778032ddd5a69ab80c68fa7d38d0e
Daud Iqbal <75211250+daudiqbal1@users.noreply.github.com>
2024-05-27 07:28:00 +0200

    Update modelTraining.py

--- modelTraining.py
+++ modelTraining.py
@@ -2,7 +2,7 @@ import os
 from ultralytics import YOLO
 
 # Loading a model, here I am using YOLO nano for our use case, we will build a new model from scratch using this classification
-model = YOLO("yolov8s.yaml")
+model = YOLO("yolov8n.yaml")
 
 #creating the model using the prepared dataset
 results = model.train(data=os.path.join(ROOT_DIR, "data.yaml"), epochs=300) #epochs defines the iterations performed to train the model
